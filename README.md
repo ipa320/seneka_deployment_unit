b04b54b8d47c068a925c642a143c181468f0a808
ipa-cmm-mn <matthiasnoesner@gmail.com>
2013-12-09 10:10:15 +0100

    updated Readme

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ SENEKA_DEPLOYMENT_UNIT
 ======================
 
 This repository contains source code for the deployment units developed at Fraunhofer IPA in the SENEKA project.
+The package is made to work with ros groovy and gazebo version 1.5
+
+----------------------------------------------------------------------
+PACKAGES:
+Functionality packages:
+seneka_pnp_planer (not working) -> pick and place planner for the deployment of the sensor node
+seneka_sensornode_detection (not working) -> starts the cob_fiducial node and manages the detection process
+
+Description packages:
+seneka_ugv_description -> Quanjo description files and gazebo launch 
+seneka_moveit_config -> moveit config for Quanjo
+universal_robot -> configured universal robot stack for seneka project
+----------------------------------------------------------------------
+
+Install:
+git clone https://github.com/Equanox/cob_object_perception/tree/seneka_fiducials (rosbuild package)
+git clone https://github.com/Equanox/seneka_deployment_unit (catkin package)
+
+Getting started with Quanjo in gazebo:
+roslaunch seneka_ugv_description ugv.launch
+roslaunch seneka_moveit_config move_group.launch
+roslaunch seneka_moveit_config moveit_rviz.launch
+
+
+
--- seneka_sensornode_detection/src/sensornode_detection.cpp
+++ seneka_sensornode_detection/src/sensornode_detection.cpp
@@ -1,20 +1,37 @@
+/**********************
+* Author: Matthias Nösner
+*
+*
+* 
+********************** */
+
 #include <ros/ros.h>
 
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
 
-tf::TransformListener *tran;
-
 int main( int argc, char** argv )
 {
-  ros::init(argc,argv,"sensornode_detection");
-  ros::NodeHandle n;
- 
-  //ros::ServiceClient client = n.ServiceClient<tf::TransformListener>("/fiducials/fiducials/tf_frames");
-
-  if(ros::service::call("/fiducials/fiducials/tf_frames", true)){
-    printf( "\nHello World\n\n" );
+  ros::init(argc, argv, "sensornode_detection");
+  ros::NodeHandle node;
+  
+  tf::TransformListener listener;
+  
+  ros::Rate rate(10.0);
+ w cobhile (node.ok()){
+    tf::StampedTransform transform;
+    try{
+      listener.lookupTransform("/fiducials/fiducials/tf_frames","/fiducials/fiducials/tf_framesGGG",ros::Time(0), transform);
+    }
+    catch (tf::TransformException ex){
+      ROS_ERROR("%s",ex.what());
+    }
+    
+    printf( "\n Y: %d", transform.getOrigin().y());
+    printf( " X: %d \n\n", transform.getOrigin().x());
+    rate.sleep();
   }
 
-
+  return 0;
 }
+//ros::ServiceClient client = n.ServiceClient<tf::TransformListener>("/fiducials/fiducials/tf_frames");
