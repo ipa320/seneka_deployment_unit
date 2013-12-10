92a480e8eeffefa25a310ec12c25193be51b38a8
ipa-cmm-mn <matthiasnoesner@gmail.com>
2013-12-10 16:38:36 +0100

    getting fiducials by custom fiducial message

--- seneka_sensornode_detection/src/sensornode_detection.cpp
+++ seneka_sensornode_detection/src/sensornode_detection.cpp
@@ -7,31 +7,21 @@
 
 #include <ros/ros.h>
 
-#include <tf/tf.h>
-#include <tf/transform_listener.h>
+#include <seneka_msgs/FiducialArray.h>
+#include <seneka_msgs/Fiducial.h>
+
+void chatterCallback(const seneka_msgs::FiducialArray::ConstPtr& msg)
+{
+  ROS_INFO("I heard: [%d]",msg->container.size());
+}
 
 int main( int argc, char** argv )
 {
   ros::init(argc, argv, "sensornode_detection");
   ros::NodeHandle node;
+  ros::Subscriber sub = node.subscribe("/fiducials/fiducial_custom_array", 1000, chatterCallback);
   
-  tf::TransformListener listener;
-  
-  ros::Rate rate(10.0);
- w cobhile (node.ok()){
-    tf::StampedTransform transform;
-    try{
-      listener.lookupTransform("/fiducials/fiducials/tf_frames","/fiducials/fiducials/tf_framesGGG",ros::Time(0), transform);
-    }
-    catch (tf::TransformException ex){
-      ROS_ERROR("%s",ex.what());
-    }
-    
-    printf( "\n Y: %d", transform.getOrigin().y());
-    printf( " X: %d \n\n", transform.getOrigin().x());
-    rate.sleep();
-  }
-
+  ros::spin();
   return 0;
 }
 //ros::ServiceClient client = n.ServiceClient<tf::TransformListener>("/fiducials/fiducials/tf_frames");
